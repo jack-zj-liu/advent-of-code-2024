89a08440c955d5a246abef7979e5080d5ea4991a
Jack Liu <jack.liuzijia@gmail.com>
2024-12-30 09:35:00 -0800

    day 19!

---
+++ 19b_linen_layout.py
@@ -0,0 +1,39 @@
+from sys import stdin
+from collections import *
+from itertools import *
+from math import *
+from bisect import *
+from copy import * 
+import heapq
+
+
+ans = 0
+patterns = []
+towels = []
+
+for line in stdin:
+    if line == '\n':
+        break
+    patterns = line.strip().split(',')
+
+for i in range(len(patterns)):
+    patterns[i] = patterns[i].strip()
+
+for line in stdin:
+    towels.append(line.strip())
+
+for t in towels:
+    n = len(t)
+    dp = [1] + [0] * (n)
+
+    for i in range(n):
+        if dp[i] == 0:
+            continue
+
+        for p in patterns:
+            if t[i:i+len(p)] == p:
+                dp[i+len(p)] += dp[i]
+
+    ans += dp[n]
+
+print(ans)
